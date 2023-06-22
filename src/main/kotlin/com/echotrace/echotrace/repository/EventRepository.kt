64a0d9463f986990cd376144b147e39c0f7bb7c6
Marius Fagerland <mariusfagerland@yahoo.no>
2023-06-22 19:50:58 +0200

    Finish EventRepository.kt

--- src/main/kotlin/com/echotrace/echotrace/repository/EventRepository.kt
+++ src/main/kotlin/com/echotrace/echotrace/repository/EventRepository.kt
@@ -12,7 +12,6 @@ data class Event(
 class EventsRepository(
     private val namedParameterJdbcTemplate: NamedParameterJdbcTemplate
 ) {
-    // create a function to insert an event into the database
     fun insertEvent(event: Event) {
         val sql = """
             INSERT INTO echotraceschema.events (name)
@@ -23,6 +22,70 @@ class EventsRepository(
             sql,
             mapOf(
                 "name" to event.name
+            )
+        )
+    }
+
+    fun getEvents(): List<Event> {
+        val sql = """
+            SELECT * FROM echotraceschema.events
+        """.trimIndent()
+
+        return namedParameterJdbcTemplate.query(
+            sql
+        ) { rs, _ ->
+            Event(
+                id = rs.getLong("id"),
+                name = rs.getString("name")
+            )
+        }
+    }
+
+    fun getEventByName(name: String): Event? {
+        val sql = """
+            SELECT * FROM echotraceschema.events
+            WHERE name = :name
+        """.trimIndent()
+
+        return namedParameterJdbcTemplate.query(
+            sql,
+            mapOf(
+                "name" to name
+            )
+        ) { rs, _ ->
+            Event(
+                id = rs.getLong("id"),
+                name = rs.getString("name")
+            )
+        }.firstOrNull()
+    }
+
+    fun updateEvent(eventUpdated: Event) {
+        val sql = """
+            UPDATE echotraceschema.events
+            SET name = :name
+            WHERE id = :id
+        """.trimIndent()
+
+        namedParameterJdbcTemplate.update(
+            sql,
+            mapOf(
+                "id" to eventUpdated.id,
+                "name" to eventUpdated.name
+            )
+        )
+    }
+
+    fun deleteEvent(id: Long) {
+        val sql = """
+            DELETE FROM echotraceschema.events
+            WHERE id = :id
+        """.trimIndent()
+
+        namedParameterJdbcTemplate.update(
+            sql,
+            mapOf(
+                "id" to id
             )
         )
     }
--- src/test/kotlin/repository/EventsRepositoryTest.kt
+++ src/test/kotlin/repository/EventsRepositoryTest.kt
@@ -19,8 +19,6 @@ import org.springframework.transaction.annotation.Transactional
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ContextConfiguration(classes = [EchotraceApplication::class])
 @Import(EventsRepository::class)
-@Transactional
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class EventsRepositoryTest(
     @Autowired
     private val eventsRepository: EventsRepository,
@@ -29,18 +27,13 @@ class EventsRepositoryTest(
     @Autowired
     private val jdbcTemplate: JdbcTemplate
 ) {
-
-    @BeforeAll
-    fun setup() {
-        flyway.migrate()
-    }
-
     @Test
     fun `test config`() {
     }
 
     @Test
     fun `test insert event`() {
+        flyway.migrate()
         val event = Event(
             id = null,
             name = "test event"
@@ -50,14 +43,64 @@ class EventsRepositoryTest(
         assert(eventsRaw.size == 1)
     }
 
-//    @Test
-//    fun `test get events`() {
-//        val event = Event(
-//            id = null,
-//            name = "test event"
-//        )
-//        eventsRepository.insertEvent(event)
-//        val events = eventsRepository.getEvents(1)
-//
-//    }
+    @Test
+    fun `test get events`() {
+        flyway.migrate()
+        val event = Event(
+            id = null,
+            name = "test event"
+        )
+        eventsRepository.insertEvent(event)
+        val events = eventsRepository.getEvents()
+        assert(events.size == 1)
+        assert(events[0].name == "test event")
+    }
+
+    @Test
+    fun `test get event by name`() {
+        flyway.migrate()
+        val event = Event(
+            id = null,
+            name = "test event"
+        )
+        eventsRepository.insertEvent(event)
+        val eventFound = eventsRepository.getEventByName(event.name)
+        assert(eventFound != null)
+        assert(eventFound?.name ?: "" == event.name)
+    }
+
+    @Test
+    fun `test update event`() {
+        flyway.migrate()
+        val event = Event(
+            id = null,
+            name = "test event"
+        )
+        eventsRepository.insertEvent(event)
+        val eventFound = eventsRepository.getEventByName(event.name)
+
+        val eventUpdated = Event(
+            id = eventFound?.id,
+            name = "test event updated"
+        )
+        eventsRepository.updateEvent(eventUpdated)
+        val eventFoundUpdated = eventsRepository.getEventByName(eventUpdated.name)
+        assert(eventFoundUpdated != null)
+        assert(eventFoundUpdated?.name ?: "" == eventUpdated.name)
+    }
+
+    @Test
+    fun `test delete event`() {
+        flyway.migrate()
+        val event = Event(
+            id = null,
+            name = "test event"
+        )
+        eventsRepository.insertEvent(event)
+        val eventFound = eventsRepository.getEventByName(event.name)
+        assert(eventFound != null)
+        eventsRepository.deleteEvent(eventFound?.id ?: 0)
+        val eventFoundDeleted = eventsRepository.getEventByName(event.name)
+        assert(eventFoundDeleted == null)
+    }
 }
