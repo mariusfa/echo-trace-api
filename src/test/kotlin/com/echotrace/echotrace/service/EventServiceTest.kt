cefbc73cbc576a014cb11531df3e44a3db7826cf
Marius Fagerland <mariusfagerland@yahoo.no>
2023-06-25 10:49:16 +0200

    Create test for 2 inserts of events

--- src/test/kotlin/com/echotrace/echotrace/service/EventServiceTest.kt
+++ src/test/kotlin/com/echotrace/echotrace/service/EventServiceTest.kt
@@ -29,4 +29,25 @@ class EventServiceTest {
         assert(eventStored?.id != null)
         assert(eventRepository.events.size == 1)
     }
+
+    @Test
+    fun `test insert 2 events, only on creates a name`() {
+        val eventRequest = EventRequest(
+            name = "test event"
+        )
+
+        eventService.insert(eventRequest)
+        eventService.insert(eventRequest)
+
+        val nameStored = nameRepository.names[0]
+        assert(nameStored?.name == "test event")
+        assert(nameStored?.id != null)
+        assert(nameRepository.names.size == 1)
+
+        val eventStored = eventRepository.events[0]
+        assert(eventStored?.name?.id == nameStored?.id)
+        assert(eventStored?.createdAt != null)
+        assert(eventStored?.id != null)
+        assert(eventRepository.events.size == 2)
+    }
 }
