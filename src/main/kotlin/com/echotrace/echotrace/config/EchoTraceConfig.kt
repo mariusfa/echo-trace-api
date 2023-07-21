b660b7d85ffd09f197baffb060152937bc38448f
Marius Fagerland <mariusfagerland@yahoo.no>
2023-07-21 15:34:42 +0200

    Update EchoTraceConfig

---
+++ src/main/kotlin/com/echotrace/echotrace/config/EchoTraceConfig.kt
@@ -0,0 +1,21 @@
+package com.echotrace.echotrace.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class EchoTraceConfig {
+
+    @Bean
+    fun corsConfig(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:5173", "mydomain-tbd.com")
+                    .allowedMethods("*")
+            }
+        }
+    }
+}
