6a06fe3c16887aee279d9b09d710ea01cfed099f
Marius Fagerland <mariusfagerland@yahoo.no>
2023-08-09 20:49:15 +0200

    Create health controller

---
+++ src/main/kotlin/com/echotrace/echotrace/controller/HealthController.kt
@@ -0,0 +1,13 @@
+package com.echotrace.echotrace.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthController {
+
+    @GetMapping()
+    fun getHealth(): String = "OK"
+}
